c3caa86ed0d64d2069ae549232bb77b63bde5ff5
Adam Hopkins <adam@amhopkins.com>
2021-08-10 18:57:29 +0300

    Add serializer

--- sanic_ext/__init__.py
+++ sanic_ext/__init__.py
@@ -1,8 +1,10 @@
 from sanic_ext.bootstrap import Extend
+from sanic_ext.extensions.convenience.decorator import serializer
 from sanic_ext.extensions.openapi import openapi
 
 __version__ = "21.9.0a1"
 __all__ = [
     "Extend",
     "openapi",
+    "serializer",
 ]
