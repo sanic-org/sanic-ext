edd6b66b4437e0ae1ca7d995e63c5be62d341533
Adam Hopkins <adam@amhopkins.com>
2021-08-10 21:12:04 +0300

    Add some tests

---
+++ tests/ext/http/test_methods.py
@@ -0,0 +1,61 @@
+from sanic.response import empty, text
+
+from sanic_ext.bootstrap import Extend
+
+
+def test_trace_and_connect_available(app):
+    @app.route("/", methods=["trace", "connect"])
+    async def handler(_):
+        return empty()
+
+    _, response = app.test_client.request("", http_method="trace")
+    assert response.status == 204
+    _, response = app.test_client.request("", http_method="connect")
+    assert response.status == 204
+    _, response = app.test_client.request("", http_method="get")
+    assert response.status == 405
+
+
+def test_auto_head(app, get_docs):
+    @app.get("/foo")
+    async def foo_handler(_):
+        return text("...")
+
+    _, response = app.test_client.head("/foo")
+    assert response.status == 200
+    assert len(response.body) == 0
+    assert int(response.headers["content-length"]) == 3
+
+    schema = get_docs()
+    assert "head" in schema["paths"]["/foo"]
+    assert "get" in schema["paths"]["/foo"]
+
+
+def test_auto_options(app, get_docs):
+    @app.post("/foo")
+    async def foo_handler(_):
+        return text("...")
+
+    _, response = app.test_client.options("/foo")
+    assert response.status == 204
+    assert len(response.body) == 0
+    assert "POST" in response.headers["allow"]
+    assert "OPTIONS" in response.headers["allow"]
+
+    schema = get_docs()
+    assert "options" in schema["paths"]["/foo"]
+    assert "post" in schema["paths"]["/foo"]
+
+
+def test_auto_trace(bare_app):
+    Extend(bare_app, config={"auto_trace": True})
+
+    @bare_app.get("/foo")
+    async def foo_handler(_):
+        return text("...")
+
+    request, response = bare_app.test_client.request(
+        "/foo", http_method="trace"
+    )
+    assert response.status == 200
+    assert response.body.startswith(request.head)
