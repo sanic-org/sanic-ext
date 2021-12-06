ec1f277f40472354f4d575754b416ab384ce175c
rocknet <chris@rocknetconsulting.com>
2021-12-06 10:43:47 -0500

    Fix hard coding of /docs in html, allow empty OAS_URL_PREFIX
    Fix hard coding of /docs in html, allow empty OAS_URL_PREFIX

--- sanic_ext/utils/route.py
+++ sanic_ext/utils/route.py
@@ -94,7 +94,7 @@ def get_all_routes(app, skip_prefix):
             if uri_filter(uri):
                 continue
 
-            if group.raw_path.startswith(skip_prefix.lstrip("/")):
+            if skip_prefix and group.raw_path.startswith(skip_prefix.lstrip("/")):
                 continue
 
             for parameter in group.params.values():
