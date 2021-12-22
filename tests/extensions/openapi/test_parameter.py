7a2d2a083f3e6732995ea9ba4a75c679adea9107
Zhiwei <zhi.wei.liang@outlook.com>
2021-12-22 07:13:06 -0700

    Temp Fix for openapi.definition with Parameter object (#27)

--- tests/extensions/openapi/test_parameter.py
+++ tests/extensions/openapi/test_parameter.py
@@ -52,8 +52,34 @@ def test_parameter_docstring(app: Sanic):
     async def handler3(request: Request, val1: int):
         return text("ok")
 
+    @app.route("/test4/<val1>")
+    @openapi.definition(
+        parameter={
+            "name": "val1",
+            "description": DESCRIPTION,
+            "required": True,
+            "schema": int,
+            "location": LOCATION,
+        }
+    )
+    async def handler4(request: Request, val1: int):
+        return text("ok")
+
+    @app.route("/test5/<val1>")
+    @openapi.definition(
+        parameter=Parameter(
+            name="val1",
+            schema=int,
+            location=LOCATION,
+            description=DESCRIPTION,
+            required=True,
+        )
+    )
+    async def handler5(request: Request, val1: int):
+        return text("ok")
+
     spec = get_spec(app)
-    for i in range(1, 4):
+    for i in range(1, 6):
         assert f"/test{i}/{{val1}}" in spec["paths"]
         parameter = spec["paths"][f"/test{i}/{{val1}}"]["get"]["parameters"][0]
         assert parameter["name"] == NAME
