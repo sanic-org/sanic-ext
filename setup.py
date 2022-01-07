3cbeea29e041449ace80675ecbea6fd795c7a3b4
Zhiwei Liang <zhi.wei.liang@outlook.com>
2022-01-07 03:56:59 -0600

    Move extras_require to setup.cfg

--- setup.py
+++ setup.py
@@ -2,22 +2,4 @@
 Sanic
 """
 from setuptools import setup
-
-
-dev_requires = ["black>=21.4b2", "flake8>=3.7.7", "isort>=5.0.0"]
-
-test_requires = [
-    "sanic_testing>=0.8",
-    "coverage",
-    "pytest",
-    "pytest-cov",
-    "tox",
-]
-
-
-setup(
-    extras_require={
-        "dev": dev_requires + test_requires,
-        "test": test_requires,
-    },
-)
+setup()
