1ec60d7ae2815ee8ed9d39a927464eb2175f49df
Zhiwei Liang <zhi.wei.liang@outlook.com>
2022-01-07 02:29:36 -0600

    Put metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 """
 Sanic
 """
-import codecs
-import os
-import re
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-install_requires = [
-    "pyyaml>=3.0.0",
-]
 
 dev_requires = ["black>=21.4b2", "flake8>=3.7.7", "isort>=5.0.0"]
 
@@ -21,43 +14,10 @@ test_requires = [
     "tox",
 ]
 
-project_root = os.path.dirname(os.path.abspath(__file__))
-
-with codecs.open(
-    os.path.join(project_root, "sanic_ext", "__init__.py"), "r", "latin1"
-) as fp:
-    try:
-        version = re.findall(
-            r"^__version__ = \"([^']+)\"\r?$", fp.read(), re.M
-        )[0]
-    except IndexError:
-        raise RuntimeError("Unable to determine version.")
-
-with open(os.path.join(project_root, "README.rst"), "r") as f:
-    long_description = f.read()
 
 setup(
-    name="sanic-ext",
-    version=version,
-    url="http://github.com/sanic-org/sanic-ext/",
-    license="MIT",
-    author="Sanic Community",
-    description="Extend your Sanic installation with some core functionality.",
-    long_description=long_description,
-    packages=find_packages(),
-    package_data={"sanic_ext": ["extensions/openapi/ui/*"]},
-    platforms="any",
-    install_requires=install_requires,
     extras_require={
         "dev": dev_requires + test_requires,
         "test": test_requires,
     },
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
 )
