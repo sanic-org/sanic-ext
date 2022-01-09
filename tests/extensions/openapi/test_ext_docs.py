1f7ca4eb757559c83fb5b0aca85ce847cfe41036
Zhiwei Liang <zhi.wei.liang@outlook.com>
2022-01-09 03:08:15 -0600

    Add external docs test cases

---
+++ tests/extensions/openapi/test_ext_docs.py
@@ -0,0 +1,61 @@
+from sanic import Request, Sanic
+from sanic.response import text
+from utils import get_spec
+
+from sanic_ext import openapi
+from sanic_ext.extensions.openapi.definitions import ExternalDocumentation
+
+
+def test_external_docs(app: Sanic):
+    @app.route("/test0")
+    @openapi.document("http://example.com/more", "Find more info here")
+    async def handler0(request: Request):
+        return text("ok")
+
+    @app.route("/test1")
+    @openapi.definition(
+        document=ExternalDocumentation(
+            "http://example.com/more", "Find more info here"
+        )
+    )
+    async def handler1(request: Request):
+        return text("ok")
+
+    @app.route("/test2")
+    @openapi.definition(document="http://example.com/more")
+    async def handler2(request: Request):
+        return text("ok")
+
+    @app.route("/test3")
+    async def handler3(request: Request):
+        """
+        openapi:
+        ---
+        summary: This is a summary.
+        externalDocs:
+          description: Find more info here
+          url: http://example.com/more
+        """
+        return text("ok")
+
+    ###### The following test case will fail and need a fix. ######
+    #
+    # @app.route("/test4")
+    # @openapi.document(
+    #     ExternalDocumentation(
+    #         "http://example.com/more", "Find more info here"
+    #     )
+    # )
+    # async def handler4(request: Request):
+    #     return text("ok")
+    #
+    ###############################################################
+
+    spec = get_spec(app)
+    paths = spec["paths"]
+    assert len(paths) == 4
+    for i in range(4):
+        doc_obj = paths[f"/test{i}"]["get"]["externalDocs"]
+        assert doc_obj["url"] == "http://example.com/more"
+        if i != 2:
+            assert doc_obj["description"] == "Find more info here"
