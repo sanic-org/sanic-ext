de7c26c606a914a7545076669eb424218cf191fc
Zhiwei Liang <zhi.wei.liang@outlook.com>
2022-01-11 23:55:02 -0600

    Add test exclude bp

--- tests/extensions/openapi/test_exclude.py
+++ tests/extensions/openapi/test_exclude.py
@@ -1,10 +1,10 @@
-from sanic import Request, Sanic, text
+from sanic import Blueprint, Request, Sanic, text
 from utils import get_spec
 
 from sanic_ext.extensions.openapi import openapi
 
 
-def test_exclude(app: Sanic):
+def test_exclude_decorator(app: Sanic):
     @app.route("/test0")
     @openapi.exclude()
     async def handler0(request: Request):
@@ -23,3 +23,29 @@ def test_exclude(app: Sanic):
     spec = get_spec(app)
     paths = spec["paths"]
     assert len(paths) == 0
+
+
+def test_exclude_bp(app: Sanic):
+    bp1 = Blueprint("blueprint1")
+    bp2 = Blueprint("blueprint2")
+
+    @bp1.route("/op1")
+    @openapi.summary("handler 1")
+    async def handler1(request: Request):
+        return text("bp1, ok")
+
+    @bp2.route("/op2")
+    @openapi.summary("handler 2")
+    async def handler2(request: Request):
+        return text("bp2, ok")
+
+    app.blueprint(bp1)
+    app.blueprint(bp2)
+
+    openapi.exclude(bp=bp1)
+    spec = get_spec(app)
+    paths = spec["paths"]
+    assert len(paths) == 1
+    assert "/op2" in paths
+    assert not "/op1" in paths
+    assert paths["/op2"]["get"]["summary"] == "handler 2"
