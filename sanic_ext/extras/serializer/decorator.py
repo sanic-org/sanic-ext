d9176f87019e23a904c57e6f0c2bacedad24ae2f
Adam Hopkins <adam@amhopkins.com>
2022-06-30 00:25:50 +0300

    Fix serializer to not break

--- sanic_ext/extras/serializer/decorator.py
+++ sanic_ext/extras/serializer/decorator.py
@@ -1,15 +1,37 @@
 from functools import wraps
-from inspect import isawaitable
+from inspect import isawaitable, signature
+
+from sanic import response
 
 
 def serializer(func, *, status: int = 200):
+    sig = signature(func)
+    simple = len(sig.parameters) == 2 or (
+        func
+        in (
+            response.HTTPResponse,
+            response.file_stream,
+            response.file,
+            response.html,
+            response.json,
+            response.raw,
+            response.redirect,
+            response.text,
+        )
+    )
+
     def decorator(f):
         @wraps(f)
         async def decorated_function(*args, **kwargs):
             retval = f(*args, **kwargs)
             if isawaitable(retval):
                 retval = await retval
-            return func(retval, status=status, *args, **kwargs)
+
+            if simple:
+                return func(retval, status=status)
+            else:
+                kwargs["status"] = status
+                return func(retval, *args, **kwargs)
 
         return decorated_function
 
--- tests/extra/test_serializer.py
+++ tests/extra/test_serializer.py
@@ -47,20 +47,20 @@ def test_serializer_with_custom(app):
     _, response = app.test_client.get("/201")
     assert response.status_code == 201
     assert response.content_type == "application/json"
- 
+
 
 def test_serializer_with_params(app):
     def message(retval, request, action, status):
         return json(
             {
-                    "request_id": str(request.id),
-                    "action": action,
-                    "message": retval,
+                "request_id": str(request.id),
+                "action": action,
+                "message": retval,
             },
             status=status,
         )
-    
-    @app.post("/<action>")
+
+    @app.get("/<action>")
     @serializer(message)
     async def do_action(request, action: str):
         return "This is a message"
