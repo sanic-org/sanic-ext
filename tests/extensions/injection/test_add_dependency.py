20ca6dd9e786f5e74389383f4422f73a5122807f
Benoit Jeaurond <benoit@jeaurond.dev>
2022-08-10 19:59:48 -0400

    chore: run formatter on changes

--- tests/extensions/injection/test_add_dependency.py
+++ tests/extensions/injection/test_add_dependency.py
@@ -246,7 +246,7 @@ def test_injection_on_websocket(app):
 
 
 def test_injection_of_awaitable_variable_in_do_cast(app):
-    ''' Test for do_cast() iscoroutine() check '''
+    """Test for do_cast() iscoroutine() check"""
 
     @app.get("/person/<name:str>")
     def handler(request, name: AsyncName):
@@ -263,7 +263,7 @@ def test_injection_of_awaitable_variable_in_do_cast(app):
 
 
 def test_injection_of_awaitable_variable_in_call(app):
-    ''' Test for __call__() iscoroutine() check '''
+    """Test for __call__() iscoroutine() check"""
 
     @app.get("/person/<name:str>")
     def handler(request, name: AsyncName):
