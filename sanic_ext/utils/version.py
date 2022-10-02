28aad85e57fc175086c72bc188da5a2da834dbcd
Adam Hopkins <adam@amhopkins.com>
2022-10-02 07:34:28 +0300

    Better version parsing (#138)

---
+++ sanic_ext/utils/version.py
@@ -0,0 +1,44 @@
+import re
+from typing import Tuple
+
+# Expression from https://github.com/pypa/packaging
+VERSION_PATTERN = r"""
+    v?
+    (?:
+        (?:(?P<epoch>[0-9]+)!)?                           # epoch
+        (?P<release>[0-9]+(?:\.[0-9]+)*)                  # release segment
+        (?P<pre>                                          # pre-release
+            [-_\.]?
+            (?P<pre_l>(a|b|c|rc|alpha|beta|pre|preview))
+            [-_\.]?
+            (?P<pre_n>[0-9]+)?
+        )?
+        (?P<post>                                         # post release
+            (?:-(?P<post_n1>[0-9]+))
+            |
+            (?:
+                [-_\.]?
+                (?P<post_l>post|rev|r)
+                [-_\.]?
+                (?P<post_n2>[0-9]+)?
+            )
+        )?
+        (?P<dev>                                          # dev release
+            [-_\.]?
+            (?P<dev_l>dev)
+            [-_\.]?
+            (?P<dev_n>[0-9]+)?
+        )?
+    )
+    (?:\+(?P<local>[a-z0-9]+(?:[-_\.][a-z0-9]+)*))?       # local version
+"""
+PATTERN = re.compile(
+    r"^\s*" + VERSION_PATTERN + r"\s*$", re.VERBOSE | re.IGNORECASE
+)
+
+
+def get_version(version) -> Tuple[int, ...]:
+    match = PATTERN.search(version)
+    if not match:
+        raise ValueError(f"Invalid version: {version}")
+    return tuple(map(int, match.group("release").split(".")))
