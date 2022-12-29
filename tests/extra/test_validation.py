57749e672006d0be629f713b8bb49157d23c6b06
Adam Hopkins <adam@amhopkins.com>
2022-12-29 09:33:15 +0200

    Add unit test

---
+++ tests/extra/test_validation.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from sys import version_info
+from typing import Literal, Union
+
+import pytest
+from sanic import text
+
+from sanic_ext import validate
+
+
+@pytest.mark.parametrize(
+    "annotation",
+    (
+        (
+            Literal["foo"],
+            Literal["foo", "bar"],
+            Union[Literal["foo"], Literal["bar"]],
+        )
+    ),
+)
+def test_literal(app, annotation):
+    @dataclass
+    class Spec:
+        name: annotation
+
+    @app.get("/")
+    @validate(query=Spec)
+    def route(_, query: Spec):
+        return text(query.name)
+
+    _, response = app.test_client.get("", params={"name": "foo"})
+    assert response.text == "foo"
+
+
+@pytest.mark.skipif(version_info < (3, 9), reason="Not needed on 3.8")
+def test_literal_3_9(app):
+    @dataclass
+    class Spec:
+        name: Literal["foo"] | Literal["bar"]
+
+    @app.get("/")
+    @validate(query=Spec)
+    def route(_, query: Spec):
+        return text(query.name)
+
+    _, response = app.test_client.get("", params={"name": "foo"})
+    assert response.text == "foo"
