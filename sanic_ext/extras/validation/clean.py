221da4ddf5ce7826f643216940d76eb5b9dcd10b
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2024-08-07 14:43:32 +0200

    fix: properly handle ignore/forbid for extra query params in pydantic validation

--- sanic_ext/extras/validation/clean.py
+++ sanic_ext/extras/validation/clean.py
@@ -3,7 +3,9 @@ from typing import Any, Dict, Type, get_origin, get_type_hints
 
 def clean_data(model: Type[object], data: Dict[str, Any]) -> Dict[str, Any]:
     hints = get_type_hints(model)
-    return {key: _coerce(hints[key], value) for key, value in data.items()}
+    return {
+        key: _coerce(hints.get(key, Any), value) for key, value in data.items()
+    }
 
 
 def _coerce(param_type, value: Any) -> Any:
