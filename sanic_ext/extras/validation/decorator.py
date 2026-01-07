b02e774286499b2896ed2ecc7a27f7fff57cc1ee
Chase Sterling <chase.sterling@h5datacenters.com>
2026-01-07 17:11:55 -0500

    Rename body_strict to strict

--- sanic_ext/extras/validation/decorator.py
+++ sanic_ext/extras/validation/decorator.py
@@ -19,7 +19,7 @@ def validate(
     query: Optional[Union[Callable[[Request], bool], type[object]]] = None,
     body_argument: str = "body",
     query_argument: str = "query",
-    body_strict: bool = True,
+    strict: bool | None = None,
 ) -> Callable[[T], T]:
     schemas = {
         key: generate_schema(param)
@@ -48,7 +48,7 @@ def validate(
                     body_argument=body_argument,
                     allow_multiple=False,
                     allow_coerce=False,
-                    strict=body_strict,
+                    strict=strict,
                 )
             elif schemas["form"]:
                 await do_validation(
